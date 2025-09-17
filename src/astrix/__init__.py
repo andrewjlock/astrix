4246ff369153fbe6071152f225508987cffe1f3e
Andrew Lock <andrew.john.lock@gmail.com>
2025-09-18 00:19:08 +1000

    Add functs to __all__

--- src/astrix/__init__.py
+++ src/astrix/__init__.py
@@ -7,8 +7,9 @@ This package provides tools for aerospace trajectory analysis and visualization.
 from . import primatives
 from . import utils
 from . import _backend_utils
+from . import functs
 
 if _backend_utils.HAS_JAX:
     _backend_utils.enforce_cpu_x64()
 
-__all__ = ['primatives', 'utils']
+__all__ = ['primatives', 'utils', 'functs']
